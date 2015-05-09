918209bef8261464f3edb97d1d839d18204cadae
underr <under@insicuri.net>
2015-05-09 06:20:59 -0300

    Redirect to previous URL path before POST

--- routes/add.js
+++ routes/add.js
@@ -15,11 +15,12 @@ var VALIDURL = /^(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?
 
 router.post('/', function(req, res) {
 
-  title = req.body.title;
-  url = req.body.url;
-  dcr = req.body.dcr || i18n.nodcr;
-  now = moment(new Date());
-  date = now.format("YYYY-MM-DD");
+  var title = req.body.title;
+  var url = req.body.url;
+  var dcr = req.body.dcr || i18n.nodcr;
+  var now = moment(new Date());
+  var date = now.format("YYYY-MM-DD");
+  var backURL = req.header('Referer') || '/?page=1&order=date';
 
   // this is not perfect, should be fixed
   if (!title || !url) {
@@ -34,7 +35,7 @@ router.post('/', function(req, res) {
     knex('bookmarks').insert({ dcr: dcr, date: date, title: title, url:url})
     .then(function(row) {
         console.log(chalk.blue(url + i18n.added + '"' + title + '"'));
-        res.redirect('/?page=1&order=date');
+        res.redirect(backURL);
       })
     .catch(function(error) {
       console.log(error);
